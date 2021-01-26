d3b5a03ff39d581e01f41a4494a4bee29b22b49d
maregor <maregor@list.ru>
2021-01-26 13:01:18 +0300

    2 commit

---
+++ library.h
@@ -0,0 +1,9 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H 1
+/* Implement a number using a linked list.&nbsp; */
+struct LinkedListNumber
+{
+struct LinkedListNumber*
+one_less_;
+};
+#endif /* DEFINITIONS_H */
